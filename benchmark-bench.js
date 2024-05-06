616f13ebe8828d34307943880accece3f899e26c
Negezor <negezor@gmail.com>
2024-05-07 03:00:33 +1100

    chore: use async import for inquirer

--- benchmark-bench.js
+++ benchmark-bench.js
@@ -1,69 +1,78 @@
 #!/usr/bin/env node
 
-const inquirer = require("inquirer");
 const bench = require("./lib/bench");
 const { choices } = require("./lib/choices");
 
+async function getInquirer() {
+  const { default: inquirer } = await import("inquirer");
+
+  return inquirer;
+}
+
 function select(callback) {
-  inquirer
-    .prompt([
-      {
-        type: "checkbox",
-        message: "Select packages",
-        name: "list",
-        choices: [...choices.map((name) => ({ name }))],
-        validate(answer) {
-          if (answer.length < 1) {
-            return "You must choose at least one package.";
-          }
-          return true;
+  getInquirer()
+    .then((inquirer) => {
+      return inquirer.prompt([
+        {
+          type: "checkbox",
+          message: "Select packages",
+          name: "list",
+          choices: [...choices.map((name) => ({ name }))],
+          validate(answer) {
+            if (answer.length < 1) {
+              return "You must choose at least one package.";
+            }
+            return true;
+          },
         },
-      },
-    ])
+      ]);
+    })
     .then((answers) => {
       callback(answers.list);
     });
 }
 
-inquirer
-  .prompt([
-    {
-      type: "confirm",
-      name: "all",
-      message: "Do you want to run all benchmark tests?",
-      default: false,
-    },
-    {
-      type: "input",
-      name: "connections",
-      message: "How many connections do you need?",
-      default: 5,
-      validate(value) {
-        return !Number.isNaN(parseFloat(value)) || "Please enter a number";
+getInquirer()
+  .then((inquirer) => {
+    return inquirer.prompt([
+      {
+        type: "confirm",
+        name: "all",
+        message: "Do you want to run all benchmark tests?",
+        default: false,
       },
-      filter: Number,
-    },
-    {
-      type: "input",
-      name: "pipelining",
-      message: "How many pipelines do you need?",
-      default: 1,
-      validate(value) {
-        return !Number.isNaN(parseFloat(value)) || "Please enter a number";
+      {
+        type: "input",
+        name: "connections",
+        message: "How many connections do you need?",
+        default: 5,
+        validate(value) {
+          return !Number.isNaN(parseFloat(value)) || "Please enter a number";
+        },
+        filter: Number,
       },
-      filter: Number,
-    },
-    {
-      type: "input",
-      name: "duration",
-      message: "How long should it take?",
-      default: 5,
-      validate(value) {
-        return !Number.isNaN(parseFloat(value)) || "Please enter a number";
+      {
+        type: "input",
+        name: "pipelining",
+        message: "How many pipelines do you need?",
+        default: 1,
+        validate(value) {
+          return !Number.isNaN(parseFloat(value)) || "Please enter a number";
+        },
+        filter: Number,
       },
-      filter: Number,
-    },
-  ])
+      {
+        type: "input",
+        name: "duration",
+        message: "How long should it take?",
+        default: 5,
+        validate(value) {
+          return !Number.isNaN(parseFloat(value)) || "Please enter a number";
+        },
+        filter: Number,
+      },
+    ]);
+  })
   .then((opts) => {
     if (!opts.all) {
       select((list) => bench(opts, list));
